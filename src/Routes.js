9dde2260760b8735667861ad1b373d0d97da9f7f
tsabit12 <tsabit830@gmail.com>
2020-06-11 16:03:16 +0700

    initial commit

---
+++ src/Routes.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Switch, Redirect } from 'react-router-dom';
+
+import { RouteWithLayout } from './components';
+import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+
+import {
+  Dashboard as DashboardView,
+  SignUp as SignUpView,
+  SignIn as SignInView,
+  NotFound as NotFoundView
+} from './views';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <RouteWithLayout
+        component={DashboardView}
+        exact
+        layout={MainLayout}
+        path="/"
+      />
+      <RouteWithLayout
+        component={SignUpView}
+        exact
+        layout={MinimalLayout}
+        path="/sign-up"
+      />
+      <RouteWithLayout
+        component={SignInView}
+        exact
+        layout={MinimalLayout}
+        path="/sign-in"
+      />
+      <RouteWithLayout
+        component={NotFoundView}
+        exact
+        layout={MinimalLayout}
+        path="/not-found"
+      />
+      <Redirect to="/not-found" />
+    </Switch>
+  );
+};
+
+export default Routes;
