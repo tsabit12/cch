c1e523219aef794d2dbcee258afb8abf6e452e4d
tsabit12 <tsabit830@gmail.com>
2020-10-05 09:03:54 +0700

    remove notification icon

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
 
 const useStyles = makeStyles(theme => ({
@@ -41,15 +40,6 @@ const Topbar = props => {
         </RouterLink>
         <div className={classes.flexGrow} />
         <Hidden mdDown>
-          <IconButton color="inherit">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
           <IconButton
             onClick={() => logout()}
             className={classes.signOutButton}
